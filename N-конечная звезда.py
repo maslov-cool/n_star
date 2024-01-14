8286a64e9c91de3d84ee84b0e1c236c586e539bd
alex <106116143+maslov-cool@users.noreply.github.com>
2024-01-14 19:23:07 +0300

    Update N-конечная звезда.py

--- N-конечная звезда.py
+++ N-конечная звезда.py
@@ -11,11 +11,12 @@ while not ((n := input()).isdigit() and int(n) > 1):
 n = int(n)
 
 for k in range(1, n + 1):
-    k1 = k + math.ceil(n / 2)
-    if k1 > n:
-        k1 -= n
+    if n % 2:
+        k1 = (k + n // 2) % n
+    else:
+        k1 = (k + n // 2 - 1) % n
     x_y_0 = (400 + r * math.cos(2 * math.pi * k / n), 400 + r * math.sin(2 * math.pi * k / n))
     x_y_1 = (400 + r * math.cos(2 * math.pi * k1 / n), 400 + r * math.sin(2 * math.pi * k1 / n))
-    canvas.create_line(x_y_0, x_y_1, fill='red')
+    canvas.create_line(x_y_0, x_y_1, fill='red',  arrowshape=(10, 20, 10))
 
 window.mainloop()
